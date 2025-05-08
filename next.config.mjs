aa22a95280a29028920dd28dd576eda2b17893dd
May Pattaranan <56375291+maysunktong@users.noreply.github.com>
2025-05-09 00:36:50 +0200

    Fix tsconfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,12 @@
-import createMDX from '@next/mdx'
- 
+import createMDX from "@next/mdx";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Configure `pageExtensions` to include markdown and MDX files
-  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
-  // Optionally, add any other Next.js config below
-}
- 
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+};
+
 const withMDX = createMDX({
   extension: /\.(md|mdx)$/,
-})
- 
-// Merge MDX config with Next.js config
-export default withMDX(nextConfig)
+});
+
+export default withMDX(nextConfig);
