96d2ec42871845f6ac821e13a0a9cf22742f1446
May Pattaranan <56375291+maysunktong@users.noreply.github.com>
2025-05-09 15:23:50 +0200

    Add : {slug: string[]}  for {slug}

--- src/app/cafe/[slug]/page.tsx
+++ src/app/cafe/[slug]/page.tsx
@@ -5,7 +5,7 @@ export default async function Page({
 }: {
   params: Promise<{ slug: string[] }>
 }) {
-  const { slug } = await params
+  const { slug }: {slug: string[]}  = await params
   const { default: Post } = await import(`@/content/cafe/${slug}.mdx`)
 
   return <Post />
