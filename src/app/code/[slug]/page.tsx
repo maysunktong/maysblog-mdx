46997378f18387d2720ceefa7a4d1f6e1a1cdddb
May Pattaranan <56375291+maysunktong@users.noreply.github.com>
2025-05-09 15:14:54 +0200

    Change whole slug files

--- src/app/code/[slug]/page.tsx
+++ src/app/code/[slug]/page.tsx
@@ -1,15 +1,18 @@
+import { codeSlugs } from "../../../lib/slugs"
+
 export default async function Page({
   params,
 }: {
-  params: { slug: string };
+  params: Promise<{ slug: string }>
 }) {
-  const { slug } = params;
+  const { slug } = await params
+  const { default: Post } = await import(`@/content/code/${slug}.mdx`)
 
-  try {
-    const { default: Post } = await import(`@/content/code/${slug}.mdx`);
-    return <Post />;
-  } catch (error) {
-    console.warn(`MDX file not found for slug: ${slug}`);
-    return null;
-  }
+  return <Post />
 }
+
+export function generateStaticParams() {
+  return codeSlugs;
+}
+
+export const dynamicParams = false
