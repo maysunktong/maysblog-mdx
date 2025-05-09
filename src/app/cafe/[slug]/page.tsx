016123d0cc5807316081c755ddf77883b8d77529
May Pattaranan <56375291+maysunktong@users.noreply.github.com>
2025-05-09 15:27:54 +0200

    add use

--- src/app/cafe/[slug]/page.tsx
+++ src/app/cafe/[slug]/page.tsx
@@ -1,11 +1,12 @@
+import { use } from 'react'
 import { cafeSlugs } from "../../../lib/slugs"
 
 export default async function Page({
   params,
 }: {
-  params: Promise<{ slug: string }>
+  params: Promise<{ slug: string[] }>
 }) {
-  const { slug }: {slug: string}  = await params
+  const { slug }: { slug: string[] } = use(params)
   const { default: Post } = await import(`@/content/cafe/${slug}.mdx`)
 
   return <Post />
